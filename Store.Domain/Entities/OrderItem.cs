5ceae5598583d98e7471035bc1b4069b72edacd6
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-20 09:40:59 -0300

    Validação da entidade 'item do pedido'

--- Store.Domain/Entities/OrderItem.cs
+++ Store.Domain/Entities/OrderItem.cs
@@ -1,9 +1,17 @@
-﻿namespace Store.Domain.Entities;
+﻿using Flunt.Validations;
+
+namespace Store.Domain.Entities;
 
 public class OrderItem : Entity
 {
     public OrderItem(Product product, int quantity)
     {
+        AddNotifications(new Contract<OrderItem>()
+            .Requires()
+            .IsNotNull(Product, nameof(Product), "Produto inválido")
+            .IsGreaterThan(quantity, 0, nameof(Quantity), "A quantidade deve ser maior do que zero")
+        );
+
         Product = product;
         Price = product is null ? 0 : product.Price;
         Quantity = quantity;
