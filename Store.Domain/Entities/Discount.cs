87a082d3c5745071081cee4151d9b36636e762e0
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-20 09:11:39 -0300

    Criação da entidade 'Discount'

---
+++ Store.Domain/Entities/Discount.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.Domain.Entities;
+
+public class Discount(decimal amount, DateTime expireDate) : Entity
+{
+    public decimal Amount { get; private set; } = amount;
+    public DateTime ExpireDate { get; private set; } = expireDate;
+
+    public bool IsValid() =>
+        DateTime.Compare(DateTime.Now, ExpireDate) < 0;
+
+    public decimal Value()
+    {
+        if(IsValid()) return Amount;
+        return 0;
+    }
+}
