af157ba72d34ce2903bb8e6bc843464c7dab276c
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-20 09:06:44 -0300

    Criação da entidade 'Product'

---
+++ Store.Domain/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Domain.Entities;
+
+public class Product(string title, decimal price, bool active) : Entity
+{
+    public string Title { get; private set; } = title;
+    public decimal Price { get; private set; } = price;
+    public bool Active { get; private set; } = active;
+}
