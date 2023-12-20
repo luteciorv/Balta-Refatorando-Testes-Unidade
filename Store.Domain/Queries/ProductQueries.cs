fe0de2db03ed8cdf3e741c4a56315eaa7f95b54b
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-20 15:28:30 -0300

    Criação das queries para produtos

---
+++ Store.Domain/Queries/ProductQueries.cs
@@ -0,0 +1,13 @@
+﻿using Store.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Store.Domain.Queries;
+
+public static class ProductQueries
+{
+    public static Expression<Func<Product, bool>> GetActiveProducts() =>
+        p => p.Active;
+
+    public static Expression<Func<Product, bool>> GetInactiveProducts() =>
+        p => !p.Active;
+}
