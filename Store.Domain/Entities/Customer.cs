079a89edf33c9d7940d7c129ddd29d7249ae6d83
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-20 09:05:10 -0300

    Criação da entidade base

--- Store.Domain/Entities/Customer.cs
+++ Store.Domain/Entities/Customer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Store.Domain.Entities;
 
-public class Customer(string name, string email)
+public class Customer(string name, string email) : Entity
 {
     public string Name { get; private set; } = name;
     public string Email { get; private set; } = email;
