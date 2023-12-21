b57851f18e2513c4e5496225781c8d4c41c76353
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-21 10:29:29 -0300

    Criação do teste de unidade em cima do 'CreateOrderCommand'

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Store.Domain.Commands;
+
+namespace Store.Tests.Commands;
+
+[TestClass]
+public class CreateOrderCommandTests
+{
+    [TestMethod]
+    public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+    {
+        IList<CreateOrderItemCommand> items = new List<CreateOrderItemCommand>
+        {
+            new(Guid.NewGuid(), 1),
+            new(Guid.NewGuid(), 1)
+        };
+        var command = new CreateOrderCommand("", "123456", "12345", items);
+
+        command.Validate();
+
+        Assert.IsFalse(command.IsValid);
+    }
+}
