0010f3dbfa6f64d39eba57f4ecaba3834cb4350d
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-21 10:32:31 -0300

    Criação da interface IHandler

---
+++ Store.Domain/Handlers/Interfaces/IHandler.cs
@@ -0,0 +1,8 @@
+﻿using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Handlers.Interfaces;
+
+public interface IHandler<TCommand> where TCommand : ICommand
+{
+    ICommandResult Handle(TCommand command);
+}
