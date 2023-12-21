00c8a1e71af6dae1b36700a5b038aa47a23e8b97
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-21 09:58:39 -0300

    Criação dos testes para as queries de produtos

---
+++ Store.Tests/Queries/ProductQueriesTests.cs
@@ -0,0 +1,38 @@
+﻿using Store.Domain.Entities;
+using Store.Domain.Queries;
+
+namespace Store.Tests.Queries;
+
+[TestClass]
+public class ProductQueriesTests
+{
+    private readonly IList<Product> _products;
+
+    public ProductQueriesTests()
+    {
+        _products = new List<Product>
+        {
+            new("Produto 01", 10, true),
+            new("Produto 02", 20, true),
+            new("Produto 03", 30, true),
+            new("Produto 04", 40, false),
+            new("Produto 05", 50, false),
+        };
+    }
+
+    [TestMethod]
+    public void Dado_a_consulta_de_produtos_ativos_deve_retornar_3()
+    {
+        var products = _products.AsQueryable().Where(ProductQueries.GetActiveProducts());
+
+        Assert.AreEqual(3, products.Count());
+    }
+
+    [TestMethod]
+    public void Dado_a_consulta_de_produtos_inativos_deve_retornar_2()
+    {
+        var products = _products.AsQueryable().Where(ProductQueries.GetInactiveProducts());
+
+        Assert.AreEqual(2, products.Count());
+    }
+}
